0d465e9976dbb3f4a50fed723c347837b7ec7f3a
Mark Jessop <vk5qi@rfhead.net>
2026-05-10 15:05:55 +0930

    More pypi fixes, maybe

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ if __name__ == "__main__":
         name="cusfpredict",
         description="Python Wrapper for the CUSF High-Altitude Balloon Predictor",
         long_description=readme,
+        long_description_content_type="text/markdown",
         version=version,
         install_requires=requirements,
         keywords=["horus balloon prediction gfs"],
