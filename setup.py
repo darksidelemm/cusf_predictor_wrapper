83051ea667547370799dc3f6cdf09b59b35d5c1d
Mark Jessop <vk5qi@rfhead.net>
2026-06-29 08:33:00 +0930

    Some fixes for pypi description

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ if __name__ == "__main__":
         packages=find_packages("."),
         classifiers=[
             "Intended Audience :: Developers",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.9"
         ]
     )
